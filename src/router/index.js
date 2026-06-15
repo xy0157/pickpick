4643515a7195c4a5348b225a7654d17be07a2644
Developer <developer@pickpick.app>
2026-06-15 18:08:09 +0800

    fix: move HomeView to src/views/ directory

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../components/views/HomeView.vue')
+    component: () => import('../views/HomeView.vue')
   }
 ]
 
